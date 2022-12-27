ee5f97e84ba112d68a5485d193d3d1eda930946d
Mounir Bennacer <48013502+Mounir-Bennacer@users.noreply.github.com>
2022-12-27 12:45:02 +0000

    formatting copilot config

--- plugins/copilot.lua
+++ plugins/copilot.lua
@@ -1,7 +1,8 @@
-local status_ok, copilot = pcall(require, "copilot")
-if not status_ok then return end
+-- local status_ok, copilot = pcall(require, "copilot")
+-- if not status_ok then return end
 
-copilot.setup {
+-- copilot.setup {
+require("copilot").setup {
   cmp = {
     enabled = true,
     method = "getCompletionsCycling",
@@ -25,14 +26,14 @@ copilot.setup {
       dismiss = "<C-]>",
     },
   },
-  ft_disable = { "markdown" },
-  server_opts_overrides = {
-    -- trace = "verbose",
-    settings = {
-      advanced = {
-        listCount = 10, -- #completions for panel
-        inlineSuggestCount = 3, -- #completions for getCompletions
-      },
-    },
-  },
+  -- ft_disable = { "markdown" },
+  -- server_opts_overrides = {
+  --   -- trace = "verbose",
+  --   settings = {
+  --     advanced = {
+  --       listCount = 10, -- #completions for panel
+  --       inlineSuggestCount = 3, -- #completions for getCompletions
+  --     },
+  --   },
+  -- },
 }
